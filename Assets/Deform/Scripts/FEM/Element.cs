b49ad8f47ee89deabd04d3f22ff49f2a89febbc2
ANDREW\yuiweit <yuiweit@andrew.cmu.edu>
2019-05-07 18:02:57 -0400

    unity init

---
+++ Assets/Deform/Scripts/FEM/Element.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Deform
+{
+    /// <summary>
+    /// Element of FEM
+    /// </summary>
+    public class Element : MonoBehaviour
+    {
+        // u1, u2, u3, u4 
+        // matrix D_u, D_x, D_v
+        // D_u = ( (u2-u1)T (u3-u1)T (u4-u1)T) 
+        // matrix Beta = D_u^(-1)
+    }
+}
---
+++ Assets/Deform/Scripts/FEM/FiniteElementModel.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Deform
+{
+    /// <summary>
+    /// Updates the FEM rigidbodies and parameters
+    /// </summary>
+    public class FiniteElementModel : MonoBehaviour
+    {
+        TetMesh meshModel; // in U space
+
+
+        public float stiffness = 1000.0f;
+
+        public float damping = 10.0f;
+
+        public int rigidBodiesCount = 0;
+
+
+        [HideInInspector]
+        public Rigidbody[] m_rigidBodies;
+
+        [HideInInspector]
+        public SpringJoint[] m_springJoints;
+
+
+        // Use this for initialization
+        void Start()
+        {
+            double[,] mat = new double[4, 4];
+            alglib.rmatrixrndorthogonal(4, out mat);
+            Debug.Log(mat);
+        }
+
+        
+    }
+}
